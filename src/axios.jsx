5d6847d5af74cda50fff38b3480035bed3ec79ad
vivek gupta <131643827+Vivekgpta@users.noreply.github.com>
2025-09-18 12:12:57 +0530

    Update axios.jsx

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}`,
+  baseURL: `https://e-kirana-shop.onrender.com/api`,
 });
 delete API.defaults.headers.common["Authorization"];
 export default API;
